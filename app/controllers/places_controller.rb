12ff87ca47380a2fee392969421d018cf6381403
wamblejosh <wamblejosh@gmail.com>
2019-03-23 22:05:15 +0000

    still working on bug

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,7 +10,7 @@ class PlacesController < ApplicationController
 	end	
 
 	def create
-		current_user.place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end	
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,3 @@
 class Place < ApplicationRecord
-	belongs_to :user
+  belongs_to :user
 end
