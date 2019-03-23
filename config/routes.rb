40365d28afa1f853d118b8cf6404704fc2a2f825
wamblejosh <wamblejosh@gmail.com>
2019-03-23 21:24:05 +0000

    fixing bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
- root 'places#index'
- resources :places
+  root 'places#index'
+  resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
