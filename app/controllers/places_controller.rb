6ed0ad453997a74824ba0535f35fa16ed8a4aad3
wamblejosh <wamblejosh@gmail.com>
2019-03-25 19:59:53 +0000

    push heroku

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.paginate(:page => params[:page], per_page: 5)
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
 	end	
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+		 redirect_to root_path
+		else
+		 render :new, status: :unprocessable_entity 
+	    end
 	end	
 
 	def show
@@ -22,22 +26,29 @@ class PlacesController < ApplicationController
         @place = Place.find(params[:id])
 
         if @place.user != current_user
-        	return render plain: 'You dont belong here...so welcome to the void', status: :forbidden
+        	return render plain: 'Not Allowed', status: :forbidden
         end	
 	end	
 
 	def update
 		@place = Place.find(params[:id])
 		if @place.user != current_user
-        	return render plain: 'You dont belong here...so welcome to the void', status: :forbidden
+        	return render plain: 'Not Allowed', status: :forbidden
 		end
 		
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+		  redirect_to root_path
+	    else 
+		  render :edit, status :unprocessable_entity
+		end   
 	end	
 
 	def destroy
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render plain: 'Not Allowed', status: :forbidden
+		end	
 		@place.destroy
 		redirect_to root_path
 	end	
