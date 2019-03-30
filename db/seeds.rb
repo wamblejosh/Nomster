e327d65c1f66e76d1d2a2d073737e6713fa07429
wamblejosh <wamblejosh@gmail.com>
2019-03-30 16:53:53 +0000

    adding validation and style to validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Place.create(name: 'teerst', address: 'tqwqswsest', description: 'tesqwsqwt', user_id: 1)
+Place.create(name: 'teqwsst2', address: 'tqwsest2', description: 'wef', user_id: 1)
+#Place.create(name: 'test3', address: 'test3', description: 'test3')
+#Place.create(name: 'test4', address: 'test4', description: 'test4')
+#Place.create(name: 'test5', address: 'test5', description: 'test5')
+#Place.create(name: 'test6', address: 'test6', description: 'test6')
