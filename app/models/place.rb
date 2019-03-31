1be2a1d2baf6e1e9ac8077c8033612f261124136
wamblejosh <wamblejosh@gmail.com>
2019-03-31 00:33:20 +0000

    workin on comment form

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+
   geocoded_by :address
-  after_validation :geocode 
+  after_validation :geocode
+   
   validates :name, presence: true, length: { minimum: 3 } 
   validates :address, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 3}
--- db/migrate/20190330231212_create_comments.rb
+++ db/migrate/20190330231212_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do |t|
+    reate_table :comments do |t|
       t.text :message
       t.string :rating
       t.integer :user_id
