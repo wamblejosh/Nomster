21a4110af46c5b079c7e8a8247fae4aeee0fd1f9
wamblejosh <wamblejosh@gmail.com>
2019-04-07 00:09:40 +0000

    updating delivery method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
 
 
   validates :message, presence: true, length: { minimum: 3 } 
@@ -16,4 +17,8 @@ class Comment < ApplicationRecord
   def humanized_rating
   	RATINGS.invert[self.rating]
   end	
+
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver_now
+  end  
 end  
